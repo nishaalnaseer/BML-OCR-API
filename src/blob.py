30477b5470b14fe9b60f259402ab7f3a5484dd53
nishaalnaseer <nishawl.naseer@outlook.com>
2024-05-23 01:11:15 +0500

    data manupulation with recursion

--- src/blob.py
+++ src/blob.py
@@ -1,6 +1,28 @@
 import json
-
+from typing import List, Any
 from icecream import ic
+
+
+def _append(array: List[dict], key: str, obj: Any) -> None:
+    next_block = obj["String"]
+    if type(next_block) is list:
+        array.extend(next_block)
+    else:
+        array.append(next_block)
+
+
+def _traverse(obj: dict | list, array: List[dict]):
+    try:
+        _append(array, "String", obj)
+    except TypeError:
+        for _obj in obj:
+            _traverse(_obj, array)
+    except KeyError:
+        try:
+            next_block = obj["TextLine"]
+            _traverse(next_block, array)
+        except KeyError:
+            pass
 
 
 def make_blocks():
@@ -10,14 +32,44 @@ def make_blocks():
     image = images_content["tests/APRIL 2022/APRIL 2022/30-04-2022/failing.jpg"]
     blocks = image["alto"]["Layout"]["Page"]["PrintSpace"]["ComposedBlock"]
 
+    string_block_array: List[Any] = []
+
     for block in blocks:
         text_block = block["TextBlock"]
-        text_line = text_block["TextLine"]
+        _traverse(text_block, string_block_array)
 
-        try:
-            ic(text_line["String"])
-        except TypeError:
-            ic(text_line)
+        #
+        #
+        # try:
+        #     text_lines = text_block["TextLine"]
+        # except TypeError:
+        #     string_block_array.append(text_block["String"])
+        #     continue
+        # # try:
+        # # text_string_block = text_line["String"]
+        #
+        # if type(text_lines) is list:
+        #     for line in text_lines:
+        #         block = line["String"]
+        #
+        #         if type(block) is list:
+        #             string_block_array.extend(block)
+        #         else:
+        #             string_block_array.append(block)
+        # else:
+        #     block = text_lines["String"]
+        #     if type(block) is list:
+        #         string_block_array.extend(block)
+        #     else:
+        #         string_block_array.append(block)
+
+    blocks = {
+        block["@CONTENT"]: block for block in
+        string_block_array
+    }
+
+    with open("out.json", 'w') as f:
+        json.dump(blocks, f, indent=4)
 
 
 make_blocks()
--- src/main.py
+++ src/main.py
@@ -2,68 +2,12 @@ import io
 from concurrent.futures import ProcessPoolExecutor
 from typing import List, Callable
 
-import cv2
 import xmltodict
 from PIL import Image
-from fastapi import FastAPI, HTTPException, UploadFile, File
-from icecream import ic
+from fastapi import FastAPI, UploadFile, File, HTTPException
 from pytesseract import pytesseract
-from PIL import Image
-import numpy as np
-
 
 app = FastAPI()
-
-
-def dissect(image_path, output_path):
-    img = cv2.imread(image_path)
-
-    # Convert the img to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # Apply edge detection method on the image
-    edges = cv2.Canny(gray, 50, 150, apertureSize=3)
-
-    # This returns an array of r and theta values
-    lines = cv2.HoughLines(edges, 1, np.pi / 180, 200)
-
-    # The below for loop runs till r and theta values
-    # are in the range of the 2d array
-    for r_theta in lines:
-        arr = np.array(r_theta[0], dtype=np.float64)
-        r, theta = arr
-        # Stores the value of cos(theta) in a
-        a = np.cos(theta)
-
-        # Stores the value of sin(theta) in b
-        b = np.sin(theta)
-
-        # x0 stores the value rcos(theta)
-        x0 = a * r
-
-        # y0 stores the value rsin(theta)
-        y0 = b * r
-
-        # x1 stores the rounded off value of (rcos(theta)-1000sin(theta))
-        x1 = int(x0 + 1000 * (-b))
-
-        # y1 stores the rounded off value of (rsin(theta)+1000cos(theta))
-        y1 = int(y0 + 1000 * (a))
-
-        # x2 stores the rounded off value of (rcos(theta)+1000sin(theta))
-        x2 = int(x0 - 1000 * (-b))
-
-        # y2 stores the rounded off value of (rsin(theta)-1000cos(theta))
-        y2 = int(y0 - 1000 * (a))
-
-        # cv2.line draws a line in img from the point(x1,y1) to (x2,y2).
-        # (0,0,255) denotes the colour of the line to be
-        # drawn. In this case, it is red.
-        cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
-
-    # All the changes made in the input image are finally
-    # written on a new image houghlines.jpg
-    cv2.imwrite('tests/linesDetected.jpg', img)
 
 
 def _image_to_string(contents: bytes) -> str:
@@ -83,9 +27,8 @@ def _image_to_json(contents: bytes) -> dict:
 
 def _image_to_data(contents: bytes) -> dict:
     image = Image.open(io.BytesIO(contents))
-    _dict = pytesseract.image_to_d(image)
+    _dict = pytesseract.image_to_data(image)
     return _dict
-
 
 
 async def process_image_in_ppe(image: UploadFile, function: Callable):
@@ -117,7 +60,7 @@ async def image_to_json(image: UploadFile = File(...)) -> dict:
 
 @app.post("/blaz", status_code=201)
 async def image_to_blaz(image: UploadFile = File(...)) -> dict:
-    text = await process_image_in_ppe(image)
+    text = await process_image_in_ppe(image, _image_to_string)
     tokens: List[str] = text.split()
 
     token = ""
@@ -130,6 +73,8 @@ async def image_to_blaz(image: UploadFile = File(...)) -> dict:
             print(tokens[index + 1])
 
             if len(token) != 16 and tokens[index + 1] == "Reference":
+                # edge case where BLAZ reference takes more than 1 line
+                # and therefore is an insufficient number of characters
                 token = token + tokens[index + 2]
                 break
 
